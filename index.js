1db28e97aa77f70f1dbe1ae9ea91775b62474d06
BinartPhotography <89169192+BinartPhotography@users.noreply.github.com>
2024-04-17 18:21:13 -0500

    Create index.js
    Automatic scroller

---
+++ index.js
@@ -0,0 +1,17 @@
+function automaticSlide() {
+  setTimeout(automaticSlide, 5000); // Update the transition time to 5 seconds (5000 milliseconds)
+
+  // Update the following lines to target images using their IDs
+  var img = [document.getElementById('slide-1'), document.getElementById('slide-2'), document.getElementById('slide-3')];
+
+  for (let pics = 0; pics < img.length; pics++) {
+    img[pics].style.display = "none";
+  }
+
+  firstIndex++;
+  if (firstIndex > img.length) {
+    firstIndex = 1;
+  }
+
+  img[firstIndex - 1].style.display = "block";
+}
