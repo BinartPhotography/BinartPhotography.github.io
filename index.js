3244826b1e39a03fa0050f32d2b681af0d56d5c0
BinartPhotography <89169192+BinartPhotography@users.noreply.github.com>
2024-04-17 18:49:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var firstIndex = 0;
+
 function automaticSlide() {
-  setTimeout(automaticSlide, 5000); // Update the transition time to 5 seconds (5000 milliseconds)
+  setTimeout(automaticSlide, 10000); // Update the transition time to 10 seconds (10000 milliseconds)
 
   // Update the following lines to target images using their IDs
   var img = [document.getElementById('slide-1'), document.getElementById('slide-2'), document.getElementById('slide-3')];
 
   for (let pics = 0; pics < img.length; pics++) {
-    img[pics].style.display = "none";
+    img[pics].style.transition = "opacity 1s ease-in-out";
+    img[pics].style.opacity = "0";
+    img[pics].classList.remove('active');
   }
+
+  img[firstIndex].style.opacity = "1";
+  img[firstIndex].classList.add('active');
 
   firstIndex++;
   if (firstIndex >= img.length) {
     firstIndex = 0;
   }
-
-  img[firstIndex].style.display = "block";
 }
 
 automaticSlide();
